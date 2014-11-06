3ff092d38d09acc85b92b89c93067abdc206763f
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-11-06 13:28:02 -0500

    added controllers

--- app/controllers/sesssions_controller.rb
+++ app/controllers/sesssions_controller.rb
@@ -1,13 +1,15 @@
 class SesssionsController < ApplicationController
 
   def create
-
+    @user = User.find_or_create_by_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to root_url
   end
 
   private
 
   def auth_hash
-
+    request.env['omniauth.auth']
   end
 
 end
