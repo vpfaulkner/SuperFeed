ec92b4d43f1321b6d2db128c0282b4ad91befbd7
GeoffPurdy <geoff_purdy@yahoo.com>
2014-11-07 15:26:43 -0500

    added twitter auth

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -2,10 +2,15 @@ class HomepagesController < ApplicationController
   def index
   if logged_in?
     github_authentication = current_user.authorizations.find_by(provider: :github)
+    twitter_authentication = current_user.authorizations.find_by(provider: :twitter)
     if github_authentication
       @github_user = HTTParty.get("https://api.github.com/user", { :body => {}, :headers => {"authorization" => "token #{github_authentication.token}", "User-Agent" => "SuperFeed"} })
       @github_username = @github_user["login"]
-      @github_response = HTTParty.get("https://api.github.com/users/#{@github_username}/received_events", { :body => {}, :headers => {"authorization" => "token #{github_authentication.token}", "User-Agent" => "SuperFeed"} })
+      @github_response = HTTParty.get("https://api.github.com/users/#{@twitter_username}/received_events", { :body => {}, :headers => {"authorization" => "token #{github_authentication.token}", "User-Agent" => "SuperFeed"} })
+    elsif
+      @twitter_user = HTTParty.get("https://api.github.com/user", { :body => {}, :headers => {"authorization" => "token #{twitter_authentication.token}", "User-Agent" => "SuperSuperFeed"} })
+      @twitter_username = @github_user["login"]
+      @twitter_response = HTTParty.get("https://api.github.com/users/#{@twitter_username}/received_events", { :body => {}, :headers => {"authorization" => "token #{twitter_authentication.token}", "User-Agent" => "SuperSuperFeed"} })
     end
   end
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo,gist,notifications,read:public_key"
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
 end
