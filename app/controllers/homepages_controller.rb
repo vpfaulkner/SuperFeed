fee31b74d2c6c194c7729f689df1c8856104e465
GeoffPurdy <geoff_purdy@yahoo.com>
2014-11-07 17:03:37 -0500

    reformatted linkedin section to be consistent with twitter and github

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -15,8 +15,9 @@ class HomepagesController < ApplicationController
       @twitter_username = @twitter_user["login"]
       @twitter_response = HTTParty.get("https://api.twitter.com/1.1/search/tweets.json#{@twitter_username}", { :body => {}, :headers => {"authorization" => "token #{twitter_authentication.token}", "User-Agent" => "SuperSuperFeed"} })
     elsif linkedin_authentication
-      @linkedin_feed = Linkedin.request_data(current_user.authorizations.find_by(provider: :linkedin))
-      @linkedin_feed.each { |message| @feed.push(message) }
+      @linkedin_user = HTTParty.get("https://api.github.com/user", { :body => {}, :headers => {"authorization" => "token #{linkedin_authentication.token}", "User-Agent" => "SuperFeed"} })
+      @linkedin_username = @github_user["login"]
+      @linkedin_response = HTTParty.get("https://api.github.com/users/#{@linkedin_username}/received_events", { :body => {}, :headers => {"authorization" => "token #{linkedin_authentication.token}", "User-Agent" => "SuperFeed"} })
     end
 
     @feed.sort_by { |message| message[:timestamp] }
