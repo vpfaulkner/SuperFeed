6a4792a5a15e2a7adeca5bd1c3a5c68df18e5a8e
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-11-07 22:22:18 -0500

    Added LinkedIn authentication

--- app/controllers/sesssions_controller.rb
+++ app/controllers/sesssions_controller.rb
@@ -1,7 +1,7 @@
 class SesssionsController < ApplicationController
 
   def create
-    @user = User.find_or_create_by_auth_hash(auth_hash)
+    @user = User.find_or_create_by_auth_hash(auth_hash, current_user)
     self.current_user = @user
     redirect_to root_url
   end
